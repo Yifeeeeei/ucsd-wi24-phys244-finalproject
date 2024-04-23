97483978187500b07f52b2b76cc6d8f626a29705
if <55980773+Yifeeeeei@users.noreply.github.com>
2024-04-23 16:00:44 -0700

    comm finished

--- comm.c
+++ comm.c
@@ -28,9 +28,10 @@ void sendAttentionHead(int dest, struct AttentionHead *attentionHead)
 struct AttentionHead receiveAttentionHead(int source)
 {
     struct AttentionHead attentionHead;
-    attentionHead.wQuery = createMatrixFrom1DArray(KEY_DIM, TKN_DIM, NULL);
-    attentionHead.wKey = createMatrixFrom1DArray(KEY_DIM, TKN_DIM, NULL);
-    attentionHead.wValue = createMatrixFrom1DArray(VAL_DIM, TKN_DIM, NULL);
+
+    attentionHead.wQuery = createZeroMatrix(KEY_DIM, TKN_DIM);
+    attentionHead.wKey = createZeroMatrix(KEY_DIM, TKN_DIM);
+    attentionHead.wValue = createZeroMatrix(VAL_DIM, TKN_DIM);
     MPI_Recv(attentionHead.wQuery.data, KEY_DIM * TKN_DIM, MPI_FLOAT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     MPI_Recv(attentionHead.wKey.data, KEY_DIM * TKN_DIM, MPI_FLOAT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     MPI_Recv(attentionHead.wValue.data, VAL_DIM * TKN_DIM, MPI_FLOAT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -47,7 +48,7 @@ void sendSequence(int dest, struct Matrix *sequence)
 struct Matrix receiveAttentionResult(int source)
 {
     // the master is expection a float matrix of size VAL_DIM * SEQ_LEN from the worker
-    struct Matrix result = createMatrixFrom1DArray(VAL_DIM, SEQ_LEN, NULL);
+    struct Matrix result = createZeroMatrix(VAL_DIM, SEQ_LEN);
     MPI_Recv(result.data, VAL_DIM * SEQ_LEN, MPI_FLOAT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     return result;
 
@@ -67,7 +68,7 @@ struct Matrix getSequence()
 
 struct Matrix receiveSequence(int source)
 {
-    struct Matrix sequence = createMatrixFrom1DArray(TKN_DIM, SEQ_LEN, NULL);
+    struct Matrix sequence = createZeroMatrix(TKN_DIM, SEQ_LEN);
     MPI_Recv(sequence.data, TKN_DIM * SEQ_LEN, MPI_FLOAT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     return sequence;
 
@@ -86,7 +87,7 @@ int main(int argc, char *argv[])
     if (numTasks < 2)
     {
         printf("Need at least 2 processes\n");
-        // MPI_Finalize();
+        MPI_Finalize();
         return 0;
     }
     printf("Rank %d of %d starting...\n", rank, numTasks);
@@ -120,6 +121,6 @@ int main(int argc, char *argv[])
         struct Matrix result = attention(&attentionHead, &sequence);
         sendAttentionResult(MASTER, &result);
     }
-    // MPI_Finalize();
+    MPI_Finalize();
     return 0;
 }
